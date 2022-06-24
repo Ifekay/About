e4462a13288380b34dff3994a5892b1157e1c188
Ifekay <olukayodeifeoluwapo@gmail.com>
2022-06-24 05:24:14 +0100

    Intial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+ 
+ let myName = "Olukayode Ifeoluwapo I"
+ let myHeight = "170 cm"
+ let myCountry = "Nigeria"
+let result = document.write(`<br> My name is: ${myName} <br><br>  Height is: ${myHeight} <br><br> I am from ${myCountry}` )
+let result2 = alert(` My name is: ${myName} \n \n Height is: ${myHeight} \n \n I am from ${myCountry}` )
+let result3 = console.log(` My name is: ${myName} \n \n  Height is: ${myHeight} \n \n I am from ${myCountry}` )
+ 
